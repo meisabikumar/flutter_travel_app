cc6bddf39e43dccc897050b0b12de2b3fe3225ff
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2021-11-30 16:56:07 +0530

    image slider welcome page

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,1 +1,42 @@
+// import 'package:flutter/material.dart';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  // WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    "welcome-one.png",
+    "welcome-two.png",
+    "welcome-three.png",
+    "welcome-4.jpg",
+    "welcome-5.jpg",
+    "welcome-6.jpg",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          itemBuilder: (_, index) {
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("img/" + images[index]),
+                    fit: BoxFit.cover),
+              ),
+            );
+          }),
+    );
+  }
+}
