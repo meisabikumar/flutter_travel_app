c5e7bda784f3a6c86d6512883135a220c3a21a81
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2021-12-01 02:22:59 +0530

    details page postition stacked widget
    1:52:30 - 2:03:30

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/pages/detail_page.dart';
 import 'package:travel_app/pages/navPages/main_page.dart';
 import 'package:travel_app/pages/welcome_page.dart';
 
@@ -17,6 +18,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: MainPage());
+        home: DetailPage());
   }
 }
---
+++ lib/pages/detail_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  DetailPage({Key? key}) : super(key: key);
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              right: 0,
+              child: Container(
+                width: double.maxFinite,
+                height: 300,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("img/mountain.jpeg"),
+                        fit: BoxFit.cover)),
+              ),
+            ),
+            Positioned(
+              left: 20,
+              top: 50,
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.menu),
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+                top: 270,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 500,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      )),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
