879be3ab7d1c7b0b04709cd35097689c5e003e5f
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2021-12-01 00:04:32 +0530

    TabsBar

--- lib/pages/navPages/home_page.dart
+++ lib/pages/navPages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:travel_app/widgets/app_large_text.dart';
 
 class Homepage extends StatefulWidget {
   Homepage({Key? key}) : super(key: key);
@@ -7,9 +9,69 @@ class Homepage extends StatefulWidget {
   _HomepageState createState() => _HomepageState();
 }
 
-class _HomepageState extends State<Homepage> {
+class _HomepageState extends State<Homepage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    TabController _tabController = TabController(length: 3, vsync: this);
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //top menu text
+          Container(
+            padding: const EdgeInsets.only(top: 70, left: 20),
+            child: Row(
+              children: [
+                const Icon(
+                  Icons.menu,
+                  size: 30,
+                  color: Colors.black54,
+                ),
+                Expanded(child: Container()),
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.withOpacity(0.5)),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 40),
+
+          // descover text
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: AppLargeText(text: "Discover"),
+          ),
+          const SizedBox(height: 30),
+
+          //tabBar
+          Container(
+            child: TabBar(
+                controller: _tabController,
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                tabs: const [
+                  Tab(text: "Places"),
+                  Tab(text: "Inspiration"),
+                  Tab(text: "Emotions")
+                ]),
+          ),
+          // tabBar View
+          Container(
+            height: 300,
+            width: double.maxFinite,
+            child: TabBarView(controller: _tabController, children: const [
+              Text("hi"),
+              Text("by"),
+              Text("there"),
+            ]),
+          )
+        ],
+      ),
+    );
   }
 }
