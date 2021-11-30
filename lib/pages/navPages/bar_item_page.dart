1f1d20373a664ae3be9e925069cb2796539a068e
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2021-11-30 22:28:32 +0530

    setwidgets

---
+++ lib/pages/navPages/bar_item_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class name extends StatelessWidget {
+  const name({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("bar item page"),
+      ),
+    );
+  }
+}
